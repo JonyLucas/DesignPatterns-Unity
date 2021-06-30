08caecfba30146b281f32ba4743af57aa38501a5
João Lucas <jony.lucas.br13@gmail.com>
2021-06-29 23:07:54 -0300

    Projeto de Command Pattern

---
+++ Command Pattern/Assets/Scripts/Commands/Command.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Command {
+    public abstract void Execute(Animator anim, bool reverse = false);
+}
+
+public class MoveCommand : Command {
+    public override void Execute(Animator anim, bool reverse) {
+        if(reverse)
+            anim.SetTrigger("isWalkingR");
+        else
+            anim.SetTrigger("isWalking");
+    }
+}
+
+public class JumpCommand : Command {
+    public override void Execute(Animator anim, bool reverse) {
+        if(reverse)
+            anim.SetTrigger("isJumpingR");
+        else
+            anim.SetTrigger("isJumping");
+    }
+}
+
+public class PunchCommand : Command {
+    public override void Execute(Animator anim, bool reverse) {
+        if(reverse)
+            anim.SetTrigger("isPunchingR");
+        else
+            anim.SetTrigger("isPunching");
+    }
+}
+
+public class KickCommand : Command {
+    public override void Execute(Animator anim, bool reverse) {
+        if(reverse)
+            anim.SetTrigger("isKickingR");
+        else
+            anim.SetTrigger("isKicking");
+    }
+}
---
+++ Command Pattern/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject actor;
+
+    private Animator animator;
+
+    private Command keySpace, keyP, keyK, keyW;
+
+    private List<Command> commands = new List<Command>();
+    private bool isReplaying = false;
+    private bool startReplay = false;
+    private Coroutine replayCoroutine;
+
+
+    // Start is called before the first frame update
+    void Start() {
+        animator = actor.GetComponent<Animator>();
+
+        keySpace = new JumpCommand();
+        keyP = new PunchCommand();
+        keyK = new KickCommand();
+        keyW = new MoveCommand();
+
+        Camera.main.GetComponent<CameraFollow360>().player = actor.transform;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (!isReplaying)
+            InputHandle();
+
+        ReplayCommands();
+
+    }
+
+    private void InputHandle() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            keySpace.Execute(animator);
+            commands.Add(keySpace);
+        }
+
+        if (Input.GetKeyDown(KeyCode.P)) {
+            keyP.Execute(animator);
+            commands.Add(keyP);
+        }
+
+        if (Input.GetKeyDown(KeyCode.K)) {
+            keyK.Execute(animator);
+            commands.Add(keyK);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W)) {
+            keyW.Execute(animator);
+            commands.Add(keyW);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+            startReplay = true;
+
+        if (Input.GetKeyDown(KeyCode.U))
+            UndoCommand();
+            
+    }
+
+    private void UndoCommand() {
+        if(commands.Count > 0) {
+            Command c = commands[commands.Count - 1];
+            c.Execute(animator, true);
+            commands.Remove(c);
+        }
+        
+    }
+
+    private void ReplayCommands() {
+
+        if(startReplay && commands.Count > 0) {
+
+            startReplay = false;
+            if (replayCoroutine != null)
+                StopCoroutine(replayCoroutine);
+
+            replayCoroutine = StartCoroutine(ReplayCommandsCoroutine());
+
+        }
+
+    }
+
+    private IEnumerator ReplayCommandsCoroutine() {
+
+        isReplaying = true;
+        int size = commands.Count;
+
+        for(int i = 0; i < size; i++) {
+            commands[i].Execute(animator);
+            yield return new WaitForSeconds(1);
+        }
+
+        isReplaying = false;
+
+    }
+}
