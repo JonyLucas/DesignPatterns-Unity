a51758f4d695270b9a94a30d2f87093a60e20a50
João Lucas <jony.lucas.br13@gmail.com>
2021-06-30 16:41:07 -0300

    Padrão singleton

---
+++ Singleton Pattern/Assets/AIControl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour {
+
+    NavMeshAgent agent;
+    Animator anim;
+    Vector3 lastGoal;
+
+    // Use this for initialization
+    void Start() {
+        
+        agent = this.GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        anim.SetBool("isWalking", true);
+
+        Vector3 lastGoal = GameEnvironment.Singleton.GetRandomGoal().transform.position;
+        PickGoalLocation();
+    }
+
+    void PickGoalLocation() {
+        lastGoal = agent.destination;
+        Vector3 newDestination = GameEnvironment.Singleton.GetRandomGoal().transform.position;
+        agent.SetDestination(newDestination);
+    }
+
+
+    // Update is called once per frame
+    void Update() {
+        if (agent.remainingDistance < 1) { // Reach the goal
+            PickGoalLocation();
+        }
+
+        CheckObstacle();        
+
+    }
+
+    private void CheckObstacle() {
+        int size = GameEnvironment.Singleton.Obstacles.Count;
+        List<GameObject> obstacles = GameEnvironment.Singleton.Obstacles;
+        for (int i = 0; i < size; i++) {
+            float distance = Vector3.Distance(obstacles[i].transform.position, transform.position);
+            if (distance < 5 && Random.Range(0,100) < 5) {
+                agent.SetDestination(lastGoal);
+            }else if(distance < 1) {
+                GameEnvironment.Singleton.RemoveObstacle(obstacles[i]);
+                break;
+            }
+        }
+    }
+
+}
---
+++ Singleton Pattern/Assets/GameEnvironment.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class implements the singleton pattern, to make a single instance of an object of this class 
+/// that will hold information about the elements of the environment.
+/// </summary>
+public sealed class GameEnvironment {
+
+    private static GameEnvironment instance;
+    private List<GameObject> obstacles = new List<GameObject>();
+    private List<GameObject> goalLocations = new List<GameObject>(GameObject.FindGameObjectsWithTag("goal"));
+
+    public List<GameObject> Obstacles { get { return obstacles; } }
+    public List<GameObject> Goals { get { return goalLocations; } }
+
+    public static GameEnvironment Singleton {
+
+        get {
+            if (instance == null) {
+                instance = new GameEnvironment();
+            }                
+
+            return instance;
+        }
+        
+    }
+
+    public void AddObstacle(GameObject obstacle) {
+        obstacles.Add(obstacle);
+    }
+
+    public void RemoveObstacle(GameObject obstacle) {
+        int index = obstacles.IndexOf(obstacle);
+        obstacles.RemoveAt(index);
+        GameObject.Destroy(obstacle);
+    }
+
+    public GameObject GetRandomGoal() {
+        return goalLocations[Random.Range(0, goalLocations.Count)];
+    }
+
+}
